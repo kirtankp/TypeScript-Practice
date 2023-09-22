c6012cc24b08783c719954647eccb7d221a5ce85
kirtan <kirtan1902@gmail.com>
2023-09-22 18:53:13 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+class UserAccount {
+    constructor(name, id) {
+        this.name = name;
+        this.id = id;
+    }
+}
+const user = new UserAccount('kirtan', 1);
+console.log(user);
+//# sourceMappingURL=index.js.map
